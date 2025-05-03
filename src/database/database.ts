9063c972eaa57d680c9a83f964bf4ef506bfb3da
AlexisFM15 <maximoalexisflorianmatos@gmail.com>
2025-05-03 04:43:50 -0400

    mongo atlas

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,5 +1,7 @@
 import Mongoose from 'mongoose'
 
+const uri =
+  'mongodb+srv://malexfuture:malex1524@cluster0.m2yho7v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 export async function DBconect() {
-  await Mongoose.connect('mongodb://mongo/music-videoPlayer', {})
+  await Mongoose.connect(uri, {})
 }
