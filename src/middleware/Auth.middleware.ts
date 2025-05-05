f92c0f5f88667ac18c12e41ac9fac7950cec42c1
AlexisFM15 <maximoalexisflorianmatos@gmail.com>
2025-05-05 16:19:48 -0400

    CI in pull_requests 2

--- src/middleware/Auth.middleware.ts
+++ src/middleware/Auth.middleware.ts
@@ -12,6 +12,7 @@ export const AuthRequired: RequestHandler = (req, res, next) => {
     const token = req.cookies['token']
     if (!token) return res.status(401).send('Invalid token, Access denied')
     const payload = jwt.verify(token, 'alexis') as IPayload
+
     req.body = payload.user
     return next()
   } catch (error) {
