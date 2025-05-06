b7b6f6821d50291ee64d378a120c451fa54779ec
AlexisFM15 <maximoalexisflorianmatos@gmail.com>
2025-05-06 16:33:00 -0400

    new changes

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,62 +1,72 @@
-import { Request, Response } from "express";
-import User from "../models/User.model";
-import { hashPassword } from "../libs/bcrypt";
+import { Request, Response } from 'express'
+import User from '../models/User.model'
+import { hashPassword } from '../libs/bcrypt'
 
-export async function getUsers(_req: Request, res: Response): Promise<Response>{
-    const user = await User.find()
+export async function getUsers(
+  _req: Request,
+  res: Response
+): Promise<Response> {
+  const user = await User.find()
 
-    return res.json({
-        user,
-        message:"Users List"
-    })
+  return res.json({
+    user,
+    message: 'Users List',
+  })
 }
 
-export async function getUser(req: Request, res: Response): Promise<Response>{
-    const user = await User.findById(req.params.id)
+export async function getUser(req: Request, res: Response): Promise<Response> {
+  const user = await User.findById(req.params.id)
 
-    return res.json({
-        user,
-        message:"user Found"
-    })
+  return res.json({
+    user,
+    message: 'user Found',
+  })
 }
 
-export async function createUser(req:Request, res: Response): Promise<Response> {
-    const {user, password} = req.body
+export async function createUser(
+  req: Request,
+  res: Response
+): Promise<Response> {
+  const { user, password } = req.body
 
-    const newUser= {
-            user:user,
-            password: hashPassword(password)
-    }
-    console.log(hashPassword(password))
-    const user1 = new User(newUser)
-    await user1.save()  
-    return res.json({
-        newUser
-    })
+  const newUser = {
+    user: user,
+    password: hashPassword(password),
+  }
+  console.log(hashPassword(password))
+  const user1 = new User(newUser)
+  await user1.save()
+  return res.json({
+    newUser,
+  })
 }
 
-export async function deleteUser(req:Request, res: Response): Promise<Response> {
-    const user = await User.findByIdAndDelete(req.params.id)
+export async function deleteUser(
+  req: Request,
+  res: Response
+): Promise<Response> {
+  const user = await User.findByIdAndDelete(req.params.id)
 
-    return res.json({
-        user,
-        message: "user Deleted"
-    })
-    
+  return res.json({
+    user,
+    message: 'user Deleted',
+  })
 }
 
-export async function UpdateUser(req:Request , res: Response): Promise<Response>{
-    const {user,password} = req.body
-    
-    const userToUpdate = {
-        user: user,
-        password: hashPassword(password)
-    }
-    const userChanged = await User.findByIdAndUpdate(req.params.id,userToUpdate)
+export async function UpdateUser(
+  req: Request,
+  res: Response
+): Promise<Response> {
+  const { user, password } = req.body
 
-    return res.json({
-        userChanged,
-        message:"user updated"
-    })
+  const userToUpdate = {
+    user: user,
+    password: hashPassword(password),
+  }
+  const userChanged = await User.findByIdAndUpdate(req.params.id, userToUpdate)
+
+  return res.json({
+    userChanged,
+    message: 'user updated',
+  })
 }
-
