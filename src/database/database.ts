86b4f9b9fc10633220db9c342324f543506bc9a4
AlexisFM15 <maximoalexisflorianmatos@gmail.com>
2025-05-06 01:55:33 -0400

    testing env vercel

--- src/database/database.ts
+++ src/database/database.ts
@@ -3,8 +3,9 @@ import Mongoose from 'mongoose'
 const uricloud =
   'mongodb+srv://malexfuture:malex1524@cluster0.m2yho7v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
-const uri = 'mongodb://localhost/music-videoPlayer'
+const uri = process.env.DB_URI!
+// 'mongodb://localhost/music-videoPlayer'
 
 export async function DBconect() {
-  await Mongoose.connect(uricloud, {})
+  await Mongoose.connect(uri, {})
 }
